6e285dc1a6cbf5c9e17a3499b48843ac5b6ab913
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-09-15 04:03:48 +0200

    start codeing train.py

--- train.py
+++ train.py
@@ -1,3 +1,10 @@
-import argparse
+import os
 import tensorflow as tf
 import core
+
+
+class WideDeepTrain(core.dataset):
+    def load_data(self, dataset_dir, subset):
+
+        assert subset in ["train", "val"]
+        dataset_dir = os.path.join(dataset_dir, subset)
