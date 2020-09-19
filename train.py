caa14d5531c143d2e03726517c2bf1d8ca4c05e3
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-09-19 10:09:19 +0200

    remove load_dataset func

--- train.py
+++ train.py
@@ -5,16 +5,7 @@ from core.dataset import Dataset
 
 
 class WideDeepTrain(core.dataset):
-    def load_data(self, dataset_dir, file_name):
-
-        dataset_path = os.path.join(dataset_dir, file_name)
-        categorical_path = os.path.join(dataset_dir, "categorical.txt")
-
-        dataset = pd.read_csv(dataset_path)
-        categorical_variables = open(categorical_path, "r").readlines()
-        categorical_variables = [int(value) for value in categorical_variables]
-
-        return dataset, categorical_variables
+    
 
 
 def train(Model):
