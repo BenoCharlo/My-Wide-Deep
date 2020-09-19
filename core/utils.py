8f7f014d633efef4e3f9836cf9d190abd442cd52
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-09-19 10:08:44 +0200

    add load_dataset function

--- core/utils.py
+++ core/utils.py
@@ -5,6 +5,18 @@ import pandas as pd
 #################################
 
 
+def load_data(self, dataset_dir, file_name):
+
+    dataset_path = os.path.join(dataset_dir, file_name)
+    categorical_path = os.path.join(dataset_dir, "categorical.txt")
+
+    dataset = pd.read_csv(dataset_path)
+    categorical_variables = open(categorical_path, "r").readlines()
+    categorical_variables = [int(value) for value in categorical_variables]
+
+    return dataset, categorical_variables
+
+
 def cross_two_variables(variable1, variable2):
     """
     Create a cross dataset from categories from the 2 variables
