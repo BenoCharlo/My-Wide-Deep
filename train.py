7872c1d298bea4414efc8728f3854385f65d4945
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-09-19 10:09:03 +0200

    remove load_dataset function

--- train.py
+++ train.py
@@ -1,10 +1,25 @@
 import os
 import tensorflow as tf
-import core
+from core.model import Model
+from core.dataset import Dataset
 
 
 class WideDeepTrain(core.dataset):
-    def load_data(self, dataset_dir, subset):
+    def load_data(self, dataset_dir, file_name):
 
-        assert subset in ["train", "val"]
-        dataset_dir = os.path.join(dataset_dir, subset)
+        dataset_path = os.path.join(dataset_dir, file_name)
+        categorical_path = os.path.join(dataset_dir, "categorical.txt")
+
+        dataset = pd.read_csv(dataset_path)
+        categorical_variables = open(categorical_path, "r").readlines()
+        categorical_variables = [int(value) for value in categorical_variables]
+
+        return dataset, categorical_variables
+
+
+def train(Model):
+    """Train the model"""
+    dataset_train = core.dataset.Dataset()
+    dataset_val = core.dataset.Dataset()
+
+    model = Model().build_model()
