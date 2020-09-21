1b9a4fd08ed259ae0f9fdd1edcdea92908cd4f48
BenoCharlo <dokodjokwassibenocharles@yahoo.fr>
2020-09-21 05:40:57 +0200

    add train function

--- train.py
+++ train.py
@@ -2,15 +2,29 @@ import os
 import tensorflow as tf
 from core.model import Model
 from core.dataset import Dataset
-
-
-class WideDeepTrain(core.dataset):
-    
+import core.utils as utils
 
 
 def train(Model):
     """Train the model"""
-    dataset_train = core.dataset.Dataset()
-    dataset_val = core.dataset.Dataset()
 
-    model = Model().build_model()
+    train_set, categoricals = utils.load_data(args.dataset, args.file_name)
+    dataset_train = Dataset(dataset=train_set, categorical_indices=categoricals)
+    (
+        categorical_variables,
+        non_categorical_variables,
+    ) = dataset_train.get_variables_names()
+
+    print("Creating cross products dataset")
+    cross_products = dataset_train.cross_categorical_dataset()
+
+    model = Model(categorical_variables, non_categorical_variables)
+    model = model.build_model(cross_products)
+
+    print("Training model")
+    model.fit(
+        [
+            [train_set[categorical_variables], train_set[non_categorical_variables]],
+            cross_products,
+        ],
+    )
